0b3ae5ef0f6c83677f5bdb800fdfe7f7320c3201
Christoffer Palm <christoffer.palm@oddhill.se>
2016-05-13 10:34:32 +0200

    Added subject field

--- modules/message/src/Entity/Message.php
+++ modules/message/src/Entity/Message.php
@@ -4,6 +4,9 @@ namespace Drupal\message\Entity;
 
 use Drupal\Core\Entity\ContentEntityBase;
 use Drupal\Core\Entity\ContentEntityInterface;
+use Drupal\Core\Entity\EntityTypeInterface;
+use Drupal\Core\Field\BaseFieldDefinition;
+use Drupal\Core\StringTranslation\TranslatableMarkup;
 
 /**
  * @ContentEntityType(
@@ -19,4 +22,23 @@ use Drupal\Core\Entity\ContentEntityInterface;
  */
 class Message extends ContentEntityBase implements ContentEntityInterface {
 
+  /**
+   * {@inheritdoc}
+   */
+  public static function baseFieldDefinitions(EntityTypeInterface $entity_type) {
+    $fields = parent::baseFieldDefinitions($entity_type);
+
+    $fields['subject'] = BaseFieldDefinition::create('string')
+      ->setLabel(new TranslatableMarkup('Subject'))
+      ->setDescription(new TranslatableMarkup('The subject of the message.'))
+      ->setRequired(TRUE)
+      ->setSettings([
+        'max_length' => 255,
+      ])
+      ->setDisplayOptions('form', array(
+        'type' => 'string_textfield',
+      ));
+
+    return $fields;
+  }
 }
